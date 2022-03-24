25ebcf63d840761c97b29ab55647b2945bbb654f
Aman Sahu <amansahu2019@iiitkottayam.ac.in>
2022-03-25 00:21:48 +0530

    setup file and folder structure

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./App.scss";
+import { Header, Work, About, Testimonials, Footer, Skills } from "./container";
+import { Navbar } from "./components";
 
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonials />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ frontend_react/src/container/index.js
@@ -0,0 +1,10 @@
+// export { default as About} from './About/About'
+
+import About from "./About/About";
+import Footer from "./Footer/Footer";
+import Skills from "./Skills/Skills";
+import Testimonials from "./Testimonials/Testimonials";
+import Header from "./Header/Header";
+import Work from "./Work/Work";
+
+export { About, Footer, Skills, Testimonials, Header, Work };
