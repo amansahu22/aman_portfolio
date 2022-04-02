cfcf2550749968c6811f36a32e278a7e40efc941
Aman Sahu <amansahu2019@iiitkottayam.ac.in>
2022-04-02 18:09:36 +0530

    Header component build and Navbar updated

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Navbar.scss";
-import { images } from "../../constant";
+import { logo } from "../../constant/images";
 import { HiMenuAlt4, HiX } from "react-icons/hi";
 import { motion } from "framer-motion";
 
@@ -9,35 +9,39 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={images.logo} alt="Portfolio" />
+        <img src={logo} alt="Portfolio" />
       </div>
       <ul className="app__navbar-links">
         {["home", "about", "work", "skills", "contact"].map((item, index) => (
           <li key={`${item}-${index}`} className="app__flex p-text">
-            <div />
             <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
       <div className="app__navbar-menu">
-        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        <HiMenuAlt4
+          onClick={() => setToggle(true)}
+          style={{ cursor: "pointer" }}
+        />
 
         {toggle && (
           <motion.div
             whileInView={{ x: [300, 0] }}
             transition={{ duration: 0.85, ease: "easeOut" }}
           >
-            <HiX onClick={() => setToggle(false)} />
+            <HiX
+              onClick={() => setToggle(false)}
+              style={{ cursor: "pointer" }}
+            />
             <ul>
-            {["home", "about", "work", "skills", "contact"].map((item) => (
-              <li key={item}>
-                <a href={`#${item}`} onClick={() => setToggle(false)}>
-                  {item}
-                </a>
-              </li>
-            ))}
+              {["home", "about", "work", "skills", "contact"].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
             </ul>
-           
           </motion.div>
         )}
       </div>
--- frontend_react/src/constant/images.js
+++ frontend_react/src/constant/images.js
@@ -1,72 +1,33 @@
-import email from "../assets/email.png";
-import mobile from "../assets/mobile.png";
-import api from "../assets/api.png";
-import cpp from "../assets/cpp.png";
-import css from "../assets/css.png";
-import figma from "../assets/figma.png";
-import flutter from "../assets/flutter.png";
-import git from "../assets/git.png";
-import graphql from "../assets/graphql.png";
-import html from "../assets/html.png";
-import javascript from "../assets/javascript.png";
-import mu5 from "../assets/mu5.png";
-import node from "../assets/node.png";
-import python from "../assets/python.png";
-import react from "../assets/react.png";
-import redux from "../assets/redux.png";
-import sass from "../assets/sass.png";
-import typescript from "../assets/typescript.png";
-import vue from "../assets/vue.png";
-
-import about01 from "../assets/about01.png";
-import about02 from "../assets/about02.png";
-import about03 from "../assets/about03.png";
-import about04 from "../assets/about04.png";
-
-import profile from "../assets/profile.png";
-import circle from "../assets/circle.svg";
-import logo from "../assets/logo.png";
-
-import adidas from "../assets/adidas.png";
-import amazon from "../assets/amazon.png";
-import asus from "../assets/asus.png";
-import bolt from "../assets/bolt.png";
-import nb from "../assets/nb.png";
-import skype from "../assets/skype.png";
-import spotify from "../assets/spotify.png";
-
-export default {
-  email,
-  mobile,
-  api,
-  cpp,
-  css,
-  figma,
-  flutter,
-  git,
-  graphql,
-  html,
-  javascript,
-  mu5,
-  node,
-  python,
-  react,
-  redux,
-  sass,
-  typescript,
-  vue,
-  about01,
-  about02,
-  about03,
-  about04,
-  profile,
-  circle,
-  logo,
-  adidas,
-  amazon,
-  asus,
-  bolt,
-  nb,
-  skype,
-  spotify,
-};
+export { default as email } from "../assets/email.png";
+export { default as mobile } from "../assets/mobile.png";
+export { default as api } from "../assets/api.png";
+export { default as cpp } from "../assets/cpp.png";
+export { default as css } from "../assets/css.png";
+export { default as figma } from "../assets/figma.png";
+export { default as flutter } from "../assets/flutter.png";
+export { default as git } from "../assets/git.png";
+export { default as graphql } from "../assets/graphql.png";
+export { default as html } from "../assets/html.png";
+export { default as javascript } from "../assets/javascript.png";
+export { default as mu5 } from "../assets/mu5.png";
+export { default as node } from "../assets/node.png";
+export { default as python } from "../assets/python.png";
+export { default as react } from "../assets/react.png";
+export { default as redux } from "../assets/redux.png";
+export { default as sass } from "../assets/sass.png";
+export { default as typescript } from "../assets/typescript.png";
+export { default as vue } from "../assets/vue.png";
+export { default as about01 } from "../assets/about01.png";
+export { default as about02 } from "../assets/about02.png";
+export { default as about03 } from "../assets/about03.png";
+export { default as about04 } from "../assets/about04.png";
+export { default as profile } from "../assets/profile.png";
+export { default as circle } from "../assets/circle.svg";
+export { default as logo } from "../assets/logo.png";
+export { default as adidas } from "../assets/adidas.png";
+export { default as amazon } from "../assets/amazon.png";
+export { default as asus } from "../assets/asus.png";
+export { default as bolt } from "../assets/bolt.png";
+export { default as nb } from "../assets/nb.png";
+export { default as skype } from "../assets/skype.png";
+export { default as spotify } from "../assets/spotify.png";
